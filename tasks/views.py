8a8b3e468a02fd89d057446a217ede957db42762
Marvin Xia <marvinxia09@gmail.com>
2023-03-06 23:14:24 -0800

    feature 16 complete

--- tasks/views.py
+++ tasks/views.py
@@ -17,3 +17,10 @@ def create_task(request):
         form = TaskForm()
         context = {"form": form}
         return render(request, "tasks/create.html", context)
+
+
+@login_required()
+def show_my_tasks(request):
+    tasks = Task.objects.filter(assignee=request.user)
+    context = {"tasks": tasks}
+    return render(request, "tasks/mine.html", context)
